dd903374c96701163af27b57f9bf4f1284b30679
Evan Smith <evanphilipsmith@gmail.com>
2022-10-11 08:40:28 -0700

    Create new "Hello world" node

---
+++ basic_algorithm/README.md
@@ -0,0 +1,2 @@
+# Basic Algorithm (HW 1)
+This is my basic algorithm implementation for HW 1.
---
+++ basic_algorithm/src/basic_algorithm_node.py
@@ -0,0 +1,19 @@
+import time
+import rospy
+from sensor_msgs.msg import Joy
+
+class BasicAlgorithmNode:
+    def __init__(self):
+        self.pub_joy = rospy.Publisher("/joy", Joy, queue_size=1)
+
+    def run(self):
+        for _ in range(5):
+            joy_msg = Joy()
+            self.pub_joy.publish(joy_msg)
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    basic_algorithm_node = BasicAlgorithmNode()
+    rospy.init_node("basic_algorithm")
+    basic_algorithm_node.run()
