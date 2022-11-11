79fc2b6bf7a68b3c80807863fc5b83399119bda8
Evan Smith <evanphilipsmith@gmail.com>
2022-11-10 23:27:09 -0800

    Add plot labels

--- rb5_control/src/slam_plot.py
+++ rb5_control/src/slam_plot.py
@@ -2,11 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from slam import SQUARE_PATH, SQUARE_LANDMARKS, OCTAGON_PATH, OCTAGON_LANDMARKS
 
-def plot_results(path: np.ndarray, landmarks: np.ndarray):
+
+def plot_results(path: np.ndarray, landmarks: np.ndarray, title: str, data: np.ndarray = None):
     plt.plot(path[:, 0], path[:, 1])
     plt.scatter(landmarks[:, 0], landmarks[:, 1])
+    if data is not None:
+        plt.scatter(data[:, 0], data[:, 1])
+        plt.scatter(data[:, 0], data[:, 1])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(title)
     plt.show()
 
 if __name__ == '__main__':
-    # plot_results(SQUARE_PATH, SQUARE_LANDMARKS)
-    plot_results(OCTAGON_PATH, OCTAGON_LANDMARKS)
+    plot_results(SQUARE_PATH, SQUARE_LANDMARKS, "Pose Estimation - Square Path")
+    plot_results(OCTAGON_PATH, OCTAGON_LANDMARKS, "Ground Truth - Octagon Path")
